cc731f45ed77f61dfdcd52173d766a04ddba8f00
Alejandro-ZetaZeta <cazz200305@hotmail.com>
2026-05-27 12:05:33 -0500

    precommit main

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
+  reactCompiler: !isDev,
   async headers() {
     return [
       {
